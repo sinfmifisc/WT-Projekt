3ccaa45b46dc3b1e5ebed0a2bc42efb297cbc279
sINFsawiez <sara.wiezorrek@stud.fh-rosenheim.de>
2018-12-05 16:31:08 +0100

    clean part1

--- umfrage/src/App.js
+++ umfrage/src/App.js
@@ -6,14 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-		<h1>UMFRAGE</h1>
-		Username: <input type="text"></input> <br></br>
-		Password: <input type="password"></input>
-        </header>
       </div>
     );
   }
+
 }
 
 export default App;
