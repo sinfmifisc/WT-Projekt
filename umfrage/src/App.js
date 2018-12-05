fc86a04ec0be702ef4a545706bcabc47095d82d2
sINFsawiez <sara.wiezorrek@stud.fh-rosenheim.de>
2018-12-05 18:35:17 +0100

    Semamntic ui setup

--- umfrage/src/App.js
+++ umfrage/src/App.js
@@ -7,7 +7,7 @@ import LoginPage from './components/pages/LoginPage';
 class App extends Component {
   render() {
     return (
-     <div>
+     <div className="ui container">
 	   <Route path="/" exact component={HomePage}/>
 	   <Route path="/login" exact component={LoginPage}/>
 	 </div>
