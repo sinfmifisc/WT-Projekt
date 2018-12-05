eed1c269036093b0267e7304dac53a2abae9ae81
sINFsawiez <sara.wiezorrek@stud.fh-rosenheim.de>
2018-12-05 23:08:52 +0100

    Login form with client-side validation

---
+++ umfrage/src/components/forms/LoginForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import PropTypes from "prop-types";
+import InlineError from "../messages/InlineError";
+
+class LoginForm extends Component {
+	state = {
+		data: {
+			username: '',
+			email: '',
+			password: ''
+		},
+	    loading: false,
+	    errors: {},
+	}
+
+	onChange = e =>
+		this.setState({
+			data: { ...this.state.data, [e.target.name]: e.target.value}
+	});
+
+	onSubmit = () => {
+	   const errors = this.validate(this.state.data);
+	   this.setState({ errors });
+	   if(Object.keys(errors).length === 0){
+		   this.props.submit(this.state.data);
+	   }
+	};
+
+	validate = data => {
+		const errors = {};
+	    if (!data.username) errors.username = "Can't be blank";
+	    if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
+	    if (!data.password) errors.password = "Can't be blank";
+	    //if (!data.password).length <= 8) = "It is to short";
+	    //if (!data.username).length <= 4) = "You need more then 4 charecters";
+	    return errors;
+	}
+
+	render() {
+	  const { data, errors } = this.state;
+      return (
+		<Form onSubmit={this.onSubmit}>
+		  <Form.Field error={!!errors.username}>
+		    <label htmlFor="username">Username</label>
+			<input
+			  type="text"
+			  id="username"
+			  name="username"
+			  placeholder="Username1"
+			  value={data.username}
+			  onChange={this.onChange}
+		  />
+		  {errors.username && <InlineError text={errors.username} />}
+		  </Form.Field>
+		  <Form.Field error={!!errors.email}>
+		    <label htmlFor="email">Email</label>
+			<input
+			  type="email"
+			  id="email"
+			  name="email"
+			  placeholder="example@examle.com"
+			  value={data.email}
+			  onChange={this.onChange}
+		  />
+		  {errors.email && <InlineError text={errors.email} />}
+		  </Form.Field>
+		  <Form.Field error={!!errors.password}>
+		    <label htmlFor="password">Password</label>
+			<input
+			  type="password"
+			  id="password"
+			  name="password"
+			  placeholder="Make it secure"
+			  value={data.password}
+			  onChange={this.onChange}
+		  />
+		  {errors.password && <InlineError text={errors.password} />}
+		  </Form.Field>
+		  <Button primary>Login</Button>
+		</Form>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+	  submit: PropTypes.func.isRequired
+};
+
+export default LoginForm;
--- umfrage/src/components/pages/LoginPage.js
+++ umfrage/src/components/pages/LoginPage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import LoginForm from  "../forms/LoginForm";
 
 class HomePage extends Component {
+  submit = data => {
+	  console.log(data);
+  };
+
   render() {
     return (
 		<div>
-		 <h1>
-		 Login
-		 </h1>
+		 <h1> Login </h1>
+		 <LoginForm submit={this.submit} />
 		</div>
     );
   }
