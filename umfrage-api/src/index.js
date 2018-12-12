eecbcf0c5a2c93a28f33c27bce182331bd462d37
Stefan Hamberger <s.hamberger@muc.qaware.local>
2018-12-12 12:45:12 +0100

    Add database

--- umfrage-api/src/index.js
+++ umfrage-api/src/index.js
@@ -3,6 +3,32 @@ import path from 'path';
 
 const app = express();
 
+
+const mysql = require('mysql2/promise');
+const host = "localhost";
+
+
+let db  // will be set below!
+mysql.createConnection({
+		host: host,
+		user: 'root',
+		password: '',
+		database: 'test'
+		
+	})
+	.then((connection) => {
+		db = connection  // remember the db-handle!
+		return db.query('CREATE TABLE students (id varchar(10) not null, name varchar(20), primary key (id))')
+	})
+	.then((result) => {
+		console.log(result)
+		console.log('Database and table created.')
+	})
+	.catch((err) => console.log(err));
+
+
+
+
 app.post("/api/auth", (req, res) => {
 	res.status(400).json({ errors: { global: "Invalid credentials"} });
 });
