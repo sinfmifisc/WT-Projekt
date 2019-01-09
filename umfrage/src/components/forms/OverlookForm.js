b3ff8b4680b274b7999882903a29b087533b82c9
sINFmifisc <michael.fischermann@stud.fh-rosenheim.de>
2019-01-09 09:59:18 +0100

    update

--- umfrage/src/components/forms/OverlookForm.js
+++ umfrage/src/components/forms/OverlookForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+//import { Icon, Label, Menu, Table } from 'semantic-ui-react';
 
 
 class OverlookForm extends Component {
