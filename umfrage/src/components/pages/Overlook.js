18e3585a4142233c8f2bc233ef643a5fb573b1c3
sINFmifisc <michael.fischermann@stud.fh-rosenheim.de>
2019-01-14 08:20:07 +0100

    imports reduziert

--- umfrage/src/components/pages/Overlook.js
+++ umfrage/src/components/pages/Overlook.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import OverlookForm from '../forms/OverlookForm';
-import PropTypes from "prop-types";
-import { Table, Icon, Accordion, Tab, TableBody } from 'semantic-ui-react';
-import axios from 'react'
+import { Table, Icon, Accordion, TableBody } from 'semantic-ui-react';
 
 
 class Overlook extends Component {
