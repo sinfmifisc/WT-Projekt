c43c33ee43991537e5b97df2e773c3a662fa84c9
sINFsawiez <sara.wiezorrek@stud.fh-rosenheim.de>
2019-01-14 18:48:46 +0100

    Add Michis chance

--- umfrage/src/components/forms/OverlookForm.js
+++ umfrage/src/components/forms/OverlookForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import { Table } from 'semantic-ui-react';
 import { connect } from "react-redux"
 import '../../App.css';
+import { Table, Icon, Accordion, TableBody } from 'semantic-ui-react';
 import { logout } from '../../actions/auth'
 //import * as actions from '../../actions/auth'
 // <button className="ui primary big button" id="new_btn" onClick={() => newPoll()} to="/">Neue Umfrage erstellen</button>
@@ -12,28 +12,151 @@ class OverlookForm extends Component{
         this.props.logout();
     };
 
+    state = { activeIndex: 0 }
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
+
    	render(){
+        const { activeIndex } = this.state
 		return (
-            <div className="semantic ui">
-                <h1>Homepage</h1>
+                
+                <div class="semantic ui">
+                <h1>PinPoll - Polls</h1>
                 <button className="ui inverted secondary button" id="logout_btn" onClick={this.logout_handler} to="/">Logout</button>
-                <Table color="orange" selectable>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Aktuelle Umfrage:</Table.HeaderCell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.HeaderCell>Frage</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                        <Table.Row>
-                            <Table.Cell>asdf</Table.Cell>
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
-                </div>
-                );
+                <Accordion className="ui">
+                    <Accordion.Title
+                        active={activeIndex === 0}
+                        index={0}
+                        onClick={this.handleClick}
+                    ><Table color="orange" styled fixed>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Aktuelle Umfragen:</Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right"><Icon circular inverted color='orange' name='angle double down' /></Table.HeaderCell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Frage</Table.HeaderCell>
+                                    <Table.HeaderCell >Ersteller</Table.HeaderCell>
+                                    <Table.HeaderCell >Erstelldatum</Table.HeaderCell>
+                                    <Table.HeaderCell >Datum</Table.HeaderCell>
+                                    <Table.HeaderCell >Beantwortet</Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right">Teilnehnerzahlen</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                        </Table>
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 0}>
+                        <Table color="orange" size="large" inverted selectable fixed width="100%">
+                            <TableBody>
+                                <Table.Row >
+                                    <Table.Cell textAlign="left">Was ist der Sinn des Lebens?</Table.Cell>
+                                    <Table.Cell >Michael</Table.Cell>
+                                    <Table.Cell >05.01.2019</Table.Cell>
+                                    <Table.Cell >06.01.2019</Table.Cell>
+                                    <Table.Cell>Jup</Table.Cell>
+                                    <Table.Cell textAlign="right">42</Table.Cell>
+                                </Table.Row>
+                            </TableBody>
+                        </Table>
+                    </Accordion.Content>
+                </Accordion>
+                
+
+                <Accordion className="ui">
+                    <Accordion.Title
+                        active={activeIndex === 1}
+                        index={1}
+                        onClick={this.handleClick}
+                    ><Table color="olive" styled fixed>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Meine Umfragen:</Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right"><Icon circular inverted color='olive' name='angle double down' /></Table.HeaderCell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Frage</Table.HeaderCell>
+                                    <Table.HeaderCell >Ersteller</Table.HeaderCell>
+                                    <Table.HeaderCell >Erstelldatum</Table.HeaderCell>
+                                    <Table.HeaderCell >Datum</Table.HeaderCell>
+                                    <Table.HeaderCell >Beantwortet</Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right">Teilnehnerzahlen</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                        </Table>
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex ===1}>
+                        <Table color="olive" size="large" inverted selectable fixed width="100%">
+                            <TableBody>
+                                <Table.Row >
+                                    <Table.Cell textAlign="left">Was ist der Sinn des Lebens?</Table.Cell>
+                                    <Table.Cell >Michael</Table.Cell>
+                                    <Table.Cell >05.01.2019</Table.Cell>
+                                    <Table.Cell >06.01.2019</Table.Cell>
+                                    <Table.Cell>Jup</Table.Cell>
+                                    <Table.Cell textAlign="right">42</Table.Cell>
+                                </Table.Row>
+                            </TableBody>
+                        </Table>
+                    </Accordion.Content>
+                </Accordion>
+
+                <Accordion className="ui">
+                    <Accordion.Title
+                        active={activeIndex === 2}
+                        index={2}
+                        onClick={this.handleClick}
+                    ><Table color="blue" styled fixed>
+                            <Table.Header>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Abgelaufene Umfragen:</Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell ></Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right"><Icon circular inverted color='blue' name='angle double down' /></Table.HeaderCell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.HeaderCell textAlign="left">Frage</Table.HeaderCell>
+                                    <Table.HeaderCell >Ersteller</Table.HeaderCell>
+                                    <Table.HeaderCell >Erstelldatum</Table.HeaderCell>
+                                    <Table.HeaderCell >Datum</Table.HeaderCell>
+                                    <Table.HeaderCell >Beantwortet</Table.HeaderCell>
+                                    <Table.HeaderCell textAlign="right">Teilnehnerzahlen</Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                        </Table>
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === 2}>
+                        <Table color="blue" size="large" inverted selectable fixed width="100%">
+                            <TableBody>
+                                <Table.Row >
+                                    <Table.Cell textAlign="left">Was ist der Sinn des Lebens?</Table.Cell>
+                                    <Table.Cell >Michael</Table.Cell>
+                                    <Table.Cell >05.01.2019</Table.Cell>
+                                    <Table.Cell >06.01.2019</Table.Cell>
+                                    <Table.Cell>Jup</Table.Cell>
+                                    <Table.Cell textAlign="right">42</Table.Cell>
+                                </Table.Row>
+                            </TableBody>
+                        </Table>
+                    </Accordion.Content>
+                </Accordion>
+            </div>    
+            
+        );
     }
 }
 OverlookForm.propTypes = {
