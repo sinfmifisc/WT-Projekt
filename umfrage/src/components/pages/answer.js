0fdab1f213e1d748330a0209f46b838b54e1dfa9
sINFmifisc <michael.fischermann@stud.fh-rosenheim.de>
2019-01-14 18:05:17 +0100

    results and answer prototypes

---
+++ umfrage/src/components/pages/answer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Form,} from 'semantic-ui-react'
+
+class answer extends Component {
+
+    render() {
+        <div class="semantic ui">
+            <h1>PinPoll - Answer</h1>
+           
+            <Form color="blue">
+                <Form.Group>
+                    <label> Frage </label>
+                    <Form.Radio label="antwort1"></Form.Radio>
+                </Form.Group>
+                <Form.Button color="blue" inverted>
+                    Submit
+                </Form.Button>
+            </Form>
+        </div>
+}
+}
---
+++ umfrage/src/components/pages/results.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Table, TableHeader } from 'semantic-ui-react'
+
+class answers extends Component {
+
+    render() {
+        <div class="semantic ui">
+            <h1>PinPoll - Results</h1>
+            <Table color="orange">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>
+                            Frage
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            
+                        </Table.HeaderCell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.HeaderCell>
+                            Antwort
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Anzahl
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>
+                            Antwort1
+                        </Table.Cell>
+                        <Table.Cell>
+                            Anzahl Antworten
+                        </Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+
+        </div>
+    }
+}
