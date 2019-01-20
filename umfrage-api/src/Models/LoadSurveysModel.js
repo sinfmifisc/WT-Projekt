8cf1f29864f629a6ccf4ef96671095850dacd0dd
Stefan Hamberger <stefan.hamberger@stud.fh-rosenheim.de>
2019-01-20 22:23:16 +0100

    Umfragen nach Datum sortiert

--- umfrage-api/src/Models/LoadSurveysModel.js
+++ umfrage-api/src/Models/LoadSurveysModel.js
@@ -10,7 +10,7 @@ export const allOpenSurveys = (user) => pool.query(`SELECT distinct id, matter,
     FROM surveys LEFT JOIN user_has_voted_for ON surveys.id = user_has_voted_for.survey
     LEFT JOIN (SELECT survey, COUNT(*) AS count FROM user_has_voted_for GROUP BY survey) AS counted_answers ON surveys.id = counted_answers.survey
     LEFT JOIN user_is_allowed_to_vote_for ON surveys.id = user_is_allowed_to_vote_for.survey
-    WHERE user_is_allowed_to_vote_for.user_name=? AND end_at > CURRENT_TIMESTAMP`, [user, user])
+    WHERE user_is_allowed_to_vote_for.user_name=? AND end_at > CURRENT_TIMESTAMP ORDER BY end_at DESC` , [user, user])
 .then((result) => {
     return result;
 })
@@ -29,7 +29,7 @@ export const ownOpenSurveys = (user) => pool.query(`SELECT distinct id, matter,
     counted_answers.count
     FROM surveys LEFT JOIN user_has_voted_for ON surveys.id = user_has_voted_for.survey
     LEFT JOIN (SELECT survey, COUNT(*) AS count FROM user_has_voted_for GROUP BY survey) AS counted_answers ON surveys.id = counted_answers.survey
-    WHERE creator=? AND end_at > CURRENT_TIMESTAMP`, [user, user, user])
+    WHERE creator=? AND end_at > CURRENT_TIMESTAMP ORDER BY end_at DESC`, [user, user, user])
 .then((result) => {
     return result;
 })    
@@ -48,7 +48,7 @@ export const allClosedSurveys = (user) => pool.query(`SELECT distinct id, matter
     FROM surveys LEFT JOIN user_has_voted_for ON surveys.id = user_has_voted_for.survey
     LEFT JOIN (SELECT survey, COUNT(*) AS count FROM user_has_voted_for GROUP BY survey) AS counted_answers ON surveys.id = counted_answers.survey
     LEFT JOIN user_is_allowed_to_vote_for ON surveys.id = user_is_allowed_to_vote_for.survey
-    WHERE user_is_allowed_to_vote_for.user_name=? AND end_at <= CURRENT_TIMESTAMP`, [user, user])
+    WHERE user_is_allowed_to_vote_for.user_name=? AND end_at <= CURRENT_TIMESTAMP ORDER BY end_at DESC`, [user, user])
 .then((result) => {
     return result;
 })
@@ -66,7 +66,7 @@ export const ownClosedSurveys = (user) => pool.query(`SELECT distinct id, matter
     counted_answers.count
     FROM surveys LEFT JOIN user_has_voted_for ON surveys.id = user_has_voted_for.survey
     LEFT JOIN (SELECT survey, COUNT(*) AS count FROM user_has_voted_for GROUP BY survey) AS counted_answers ON surveys.id = counted_answers.survey
-    WHERE creator=? AND end_at <= CURRENT_TIMESTAMP`, [user, user, user])
+    WHERE creator=? AND end_at <= CURRENT_TIMESTAMP ORDER BY end_at DESC`, [user, user, user])
 .then((result) => {
     return result;
 })
